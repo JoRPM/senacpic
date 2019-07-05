e6001052eb03ecfb329f7db9acd2546d69c1ef8d
JoRPM <44869196+JoRPM@users.noreply.github.com>
2019-07-05 10:09:45 -0300

    dia 05/07 às 10:00
    criação do AuthService

--- mad_linguagem_scripts_web_2018.2-master/Angular/senacpic/src/app/home/signin/signin.component.ts
+++ mad_linguagem_scripts_web_2018.2-master/Angular/senacpic/src/app/home/signin/signin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './signin.component.html',  
@@ -8,13 +10,36 @@ export class SigninComponent  {
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private authService:AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      username: ['admin', Validators.required],
-      password: ['admin@123', Validators.required]
+      username: ['', Validators.required],
+      password: ['', Validators.required]
     });
+  }
+
+  login() {
+    //document.getElementById("idLogin").nodeValue()
+    const username = this.loginForm.get('username').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService.autenticacao(username,password)
+                    .subscribe(
+                        (data:any) =>{ 
+                        if(data.length > 0)
+                        console.log(data)
+                        console.log(`O usuario ${data[0].fullname} logou`)
+                        this.router.navigateByUrl('form');
+                        
+                        },
+                        error => {
+                          console.log(error);
+                          this.loginForm.reset(); // apaga o formulário
+                        }
+                    )
+
+
   }
 
 }
